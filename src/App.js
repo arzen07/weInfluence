92203724b29ad78206ac2b2506e921214dea38ea
123nishan <nishan_98@hotmail.com>
2020-10-23 21:13:16 +0545

    merge to master

--- src/App.js
+++ src/App.js
@@ -2,31 +2,43 @@ import React from 'react';
 import './App.css';
 import SignIn from './components/sign_in/SignIn';
 import SignUp from './components/sign_up/SignUp';
-import Nav from './components/Navbar';
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import { BrowserRouter as Router, Switch, Route,Redirect } from 'react-router-dom';
 import { HOME, SIGN_IN, SIGN_UP } from './core/constants';
 import firebase from "./core/firebase"
 import { AuthProvider } from './core/services/Auth';
 import PrivateRoute from './core/services/Route';
-const Home = () => {
-  return (
-    <div>
-      <h1>We Influence!!!</h1>
-      <button onClick={() => firebase.auth().signOut()}>Log out</button>
-    </div>
-  )
-}
+import Home from './components/pages/Home.jsx';
+import Plans from './components/pages/Plans.jsx';
+import Services from './components/pages/Services.jsx';
+import Footer from './components/pages/Footer.jsx';
+import About from './components/pages/About.jsx';
+import Navbar from './components/Navbar.jsx';
+// const Home = () => {
+//   return (
+//     <div>
+//       <h1>We Influence!!!</h1>
+//       <button onClick={() => firebase.auth().signOut()}>Log out</button>
+//     </div>
+//   )
+// }
 function App() {
   return (
     <AuthProvider>
       <Router>
         <div className="App">
-          <Nav />
+        <Navbar/>
           <Switch>
             <Route exact path={SIGN_IN} component={SignIn} />
             <Route exact path={SIGN_UP} component={SignUp} />
-            <PrivateRoute exact path={HOME} component={Home} />
+            {/* <PrivateRoute exact path={HOME} component={Home} /> */}
+            <Route exact path='/' component={Home}/>
+            <Route exact path='/plans' component={Plans}/>
+            <Route exact path='/services' component={Services}/>
+            <Route  exact path='/footer' component={Footer}/>
+            <Route exact path='/about' component={About}/>
+            <Redirect to= '/' />
           </Switch>
         </div>
       </Router>
--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,23 @@ import Services from './components/pages/Services.jsx';
 import Footer from './components/pages/Footer.jsx';
 import About from './components/pages/About.jsx';
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar.jsx';
 import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
-
-
+import SignIn from './components/sign_in/SignIn';
+import SignUp from './components/sign_up/SignUp';
+import { HOME, SIGN_IN, SIGN_UP } from './core/constants';
+import { AuthProvider } from './core/services/Auth';
 function App() {
   return (
     <>
+      <AuthProvider>
     <Router>
      
     <Navbar/>
     <Switch>
+    <Route exact path={SIGN_IN} component={SignIn} />
+      <Route exact path={SIGN_UP} component={SignUp} />
+      {/* <PrivateRoute exact path={HOME} component={Home} /> */}
       <Route exact path='/' component={Home}/>
       <Route exact path='/plans' component={Plans}/>
       <Route exact path='/services' component={Services}/>
@@ -27,6 +33,7 @@ function App() {
 
     </Switch>
     </Router>
+    </AuthProvider>
     </>
   );
 }
